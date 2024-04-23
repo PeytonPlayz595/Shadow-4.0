2f7cd3f93257f31e5c6af1b0c4bd8532f5d2970b
Orion Leon <121742281+orionzleon@users.noreply.github.com>
2024-04-22 17:50:54 -0700

    re-add index.html

--- MakeOfflineDownload.sh
+++ MakeOfflineDownload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp "desktopRuntime/MakeOfflineDownload.jar:desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "/dev/null" "javascript/Shadow_Client.html" "javascript/lang"
+java -cp "desktopRuntime/MakeOfflineDownload.jar:desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "/dev/null" "javascript/Shadow_Client_Offline.html" "javascript/lang"
