4a4a35c3f03158404fe89389f5a0af9edb9434d9
Orion Leon <121742281+orionzleon@users.noreply.github.com>
2024-04-24 14:12:49 -0700

    Update EaglercraftVersion.java

--- src/main/java/net/lax1dude/eaglercraft/v1_8/EaglercraftVersion.java
+++ src/main/java/net/lax1dude/eaglercraft/v1_8/EaglercraftVersion.java
@@ -49,7 +49,7 @@ public class EaglercraftVersion {
 	public static final String mainMenuStringB = projectOriginName + " " + projectOriginVersion;
 	public static String getMainMenuStringC() {
 		int loadedmods = returntotalloadedmods();
-		String mainMenuStringC = projectForkName + " " + projectForkVersion;
+		String mainMenuStringC = "EaglerForge: ";
 		if(!PlatformRuntime.isDebugRuntime()) {
 			if(loadedmods == 1) {
 				mainMenuStringC = mainMenuStringC + " (" + loadedmods + " mod loaded)";
