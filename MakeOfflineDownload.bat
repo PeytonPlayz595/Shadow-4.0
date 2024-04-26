182315cfaf5f636e67ebff74501c14d5dd80739d
Orion Leon <121742281+orionzleon@users.noreply.github.com>
2024-04-26 07:06:05 -0700

    update textures to 1.18 and move shadow client assets to /assets/shadow (#12)
    * hi
    
    * uyh
    
    * kml
    
    * jh
    
    * hj
    
    * nkj
    
    * nm
    
    * fa
    
    * afd

--- MakeOfflineDownload.bat
+++ MakeOfflineDownload.bat
@@ -1,4 +1,4 @@
 @echo off
 title MakeOfflineDownload
-java -cp "desktopRuntime/MakeOfflineDownload.jar;desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/Shadow_Client_Offline_en-US.html" "javascript/Shadow_Client_Offline.html" "javascript/lang"
+java -cp "desktopRuntime/MakeOfflineDownload.jar;desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/Shadow_Client_Offline_en_US.html" "javascript/Shadow_Client_Offline.html" "javascript/lang"
 pause
